6acde1c0aa62b8409a06068f9a201d866ef7634a
gracyvermaa <90313534+gracyvermaa@users.noreply.github.com>
2021-09-24 11:15:31 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,22 @@
+import 'package:fb_demo/TimerAppPage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        
+        primarySwatch: Colors.blue,
+      ),
+      home: TimerApp(),
+    );
+  }
+}
+
